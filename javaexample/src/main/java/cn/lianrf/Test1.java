3d73da884742a02675bf112858599e26f4d8eff8
lianrf <lianrf@chinawiserv.com>
2018-01-02 21:51:27 +0800

    first

---
+++ javaexample/src/main/java/cn/lianrf/Test1.java
@@ -0,0 +1,14 @@
+package cn.lianrf;
+
+import org.junit.Test;
+
+/**
+ * Created by lianrongfa on 2018/1/2.
+ */
+public class Test1 {
+    @Test
+    public void test(){
+        System.out.println("111");
+    }
+
+}
