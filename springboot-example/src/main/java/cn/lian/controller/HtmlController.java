65ef678b781b5821615ad7cc2d6ffc38683a2706
lianrf <870779945@qq.com>
2018-01-19 17:57:36 +0800

    template

---
+++ springboot-example/src/main/java/cn/lian/controller/HtmlController.java
@@ -0,0 +1,17 @@
+package cn.lian.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+/**
+ * Created by lianrongfa on 2018/1/19.
+ */
+@Controller
+@RequestMapping("/index")
+public class HtmlController {
+    @RequestMapping("/html")
+    public String test(){
+        return "index";
+    }
+}
