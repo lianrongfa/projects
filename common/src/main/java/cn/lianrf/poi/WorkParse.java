8bfc223fbe10f244fd2abf08a3fe4e42b477d34f
lianrf <870779945@qq.com>
2018-04-09 22:16:48 +0800

    spring boot starter ehcache

--- common/src/main/java/cn/lianrf/poi/WorkParse.java
+++ common/src/main/java/cn/lianrf/poi/WorkParse.java
@@ -1,12 +1,7 @@
 package cn.lianrf.poi;
 
 
-
-
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.ss.usermodel.*;
 
 import java.lang.reflect.Field;
 import java.lang.reflect.Method;
@@ -14,20 +9,18 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.Map;
 
-import static cn.lianrf.poi.ParseStatus.*;
-
 /**
  * Created by lianrongfa on 2018/3/22.
  */
-public class WorkParse<T> implements Parse<T>{
+public class WorkParse<T> implements Parse<T> {
 
     private Class<T> clazz;
     private Workbook workbook;
 
-    private Map<String,Method> names;
+    private Map<String, Method> names;
 
     public WorkParse(Workbook workbook, Class<T> clazz) {
-        if(clazz==null||workbook==null){
+        if (clazz == null || workbook == null) {
             throw new RuntimeException();
         }
         this.clazz = clazz;
@@ -38,31 +31,32 @@ public class WorkParse<T> implements Parse<T>{
 
     /**
      * 目前支持String类型
+     *
      * @param t
      * @return
      */
     public ParseStatus parseWork(T t) {
-        if(t==null){
-            return new ParseStatus(ParseStatus.CODE_FALSE,"");
+        if (t == null) {
+            return new ParseStatus(ParseStatus.CODE_FALSE, "");
         }
         Sheet sheet = workbook.getSheetAt(0);
 
         Iterator<Row> rowIterator = sheet.iterator();
-        while (rowIterator.hasNext()){
+        while (rowIterator.hasNext()) {
             Row row = rowIterator.next();
             Iterator<Cell> cellIterator = row.iterator();
-            while (cellIterator.hasNext()){
+            while (cellIterator.hasNext()) {
                 Cell cell = cellIterator.next();
                 cell.setCellType(CellType.STRING);
                 String name = cell.getStringCellValue();
-                if(name==null||name==""){
+                if (name == null || name == "") {
                     continue;
                 }
-                Method method = getMethod("get"+name.substring(0,1).toUpperCase()+name.substring(1));
-                if(method!=null){
+                Method method = getMethod("get" + name.substring(0, 1).toUpperCase() + name.substring(1));
+                if (method != null) {
                     try {
                         Object o = method.invoke(t, null);
-                        cell.setCellValue((String)o);
+                        cell.setCellValue((String) o);
                     } catch (Exception e) {
                         e.printStackTrace();
                     }
@@ -72,15 +66,15 @@ public class WorkParse<T> implements Parse<T>{
         return null;
     }
 
-    private void buildNames(Class<T> clazz){
+    private void buildNames(Class<T> clazz) {
         Field[] fields = clazz.getDeclaredFields();
-        this.names=new HashMap();
+        this.names = new HashMap();
         for (Field field : fields) {
             String name = field.getName();
-            name="get"+name.substring(0,1).toUpperCase()+name.substring(1);
+            name = "get" + name.substring(0, 1).toUpperCase() + name.substring(1);
             try {
                 Method method = clazz.getDeclaredMethod(name, null);
-                names.put(name,method);
+                names.put(name, method);
             } catch (NoSuchMethodException e) {
                 e.printStackTrace();
             }
@@ -88,7 +82,7 @@ public class WorkParse<T> implements Parse<T>{
         }
     }
 
-    private Method getMethod(String name){
+    private Method getMethod(String name) {
         return names.get(name);
     }
 
