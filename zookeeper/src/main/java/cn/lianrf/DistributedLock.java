1e1ab3c8027cec45481d6473487f0892dffcc812
lianrf <870779945@qq.com>
2018-06-15 20:52:16 +0800

    xml

--- zookeeper/src/main/java/cn/lianrf/DistributedLock.java
+++ zookeeper/src/main/java/cn/lianrf/DistributedLock.java
@@ -2,7 +2,10 @@ package cn.lianrf;
 
 import org.apache.zookeeper.WatchedEvent;
 import org.apache.zookeeper.Watcher;
+import org.apache.zookeeper.ZooKeeper;
 
+import java.io.IOException;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
@@ -10,7 +13,7 @@ import java.util.concurrent.locks.Lock;
 /**
  * Created by lianrongfa on 2018/6/10.
  */
-public class DistributedLock implements Lock,Watcher{
+public class DistributedLock implements Lock, Watcher {
     public void lock() {
 
     }
@@ -37,5 +40,28 @@ public class DistributedLock implements Lock,Watcher{
 
     public void process(WatchedEvent event) {
 
+    }
+
+    public static void main(String[] args) {
+        try {
+            final CountDownLatch countDownLatch = new CountDownLatch(1);
+            ZooKeeper zooKeeper = new ZooKeeper("192.168.11.153:2181, 192.168.11.154:2181,192.168.11.155:2181",
+                    4000, new Watcher() {
+                @Override
+                public void process(WatchedEvent event) {
+                    if (Event.KeeperState.SyncConnected == event.getState()) {
+                        //如果收到了服务 端的响应事件，连接成功
+                        countDownLatch.countDown();
+                    }
+                }
+            });
+            countDownLatch.await();
+            System.out.println(zooKeeper.getState());//CONNECTING
+            zooKeeper.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
     }
 }
