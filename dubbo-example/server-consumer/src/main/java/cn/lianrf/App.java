68db1d8a9f6d03e8c90aa70303cd41f712a9f228
lianrf <870779945@qq.com>
2018-06-20 18:42:23 +0800

    dubbo

--- dubbo-example/server-consumer/src/main/java/cn/lianrf/App.java
+++ dubbo-example/server-consumer/src/main/java/cn/lianrf/App.java
@@ -2,16 +2,20 @@ package cn.lianrf;
 
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+import java.io.IOException;
+
 /**
  * Created by lianrongfa on 2018/6/20.
  */
 public class App {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         ClassPathXmlApplicationContext applicationContext =
                 new ClassPathXmlApplicationContext("dubbo-consumer.xml");
 
         IUserService userService = (IUserService) applicationContext.getBean("userServiceImpl");
 
         System.out.println(userService.insert("hello"));
+
+        System.in.read();
     }
 }
